1c91ac958ab1898871141249ac08ebc37f14261c
automateyournetwork <ptcapo@gmail.com>
2024-01-15 11:26:49 -0500

    stable but wonky

--- capophied/capophied.py
+++ capophied/capophied.py
@@ -7,9 +7,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Display the device being used
 if device.type == 'cuda':
-    st.write("Using GPU")
+    st.write("Your system is using the GPU")
 else:
-    st.write("Using CPU")
+    st.write("Your system is using the CPU")
 
 # Load the model and tokenizer, and move the model to the device
 model = AutoModelForCausalLM.from_pretrained("/app/phi-2", torch_dtype="auto", trust_remote_code=True).to(device)
@@ -17,13 +17,12 @@ tokenizer = AutoTokenizer.from_pretrained("/app/phi-2", trust_remote_code=True)
 
 # Streamlit UI
 st.title("Capophied")
-prompt = st.text_area("Enter your prompt", "Where is the CN Tower and what can you tell me about it?")
-enhanced_prompt = f"Instruct: Provide a concise, factual summary about: {prompt}\nOutput:"
+prompt = st.text_area("Enter your prompt", "Where is the CN Tower?")
 
 if st.button("Generate Output"):
     with torch.no_grad():
         # Encode input and move tensors to the device
-        token_ids = tokenizer.encode(enhanced_prompt, add_special_tokens=False, return_tensors="pt").to(device)
+        token_ids = tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt").to(device)
         output_ids = model.generate(
             token_ids,
             max_new_tokens=512,
